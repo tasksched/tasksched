4665dc0df86408f66c8e14f1a021dc87b35331cf
Sébastien Helleu <flashcode@flashtux.org>
2020-11-25 07:45:47 +0100

    Version 0.2.0-dev

--- tasksched/tasksched.py
+++ tasksched/tasksched.py
@@ -28,7 +28,7 @@ from tasksched.project import Project
 from tasksched.workplan import build_workplan
 from tasksched.workplan_text import workplan_to_text
 
-__version__ = '0.1.0'
+__version__ = '0.2.0-dev'
 
 __all__ = (
     '__version__',
